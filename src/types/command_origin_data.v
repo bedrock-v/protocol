b98986aee14abd36eeb711d424266eb2c39395e1
xRookieFight <me@xrookiefight.com>
2026-06-28 21:21:43 +0300

    fix: decode command origin type as varuint with conditional player id

--- src/types/command_origin_data.v
+++ src/types/command_origin_data.v
@@ -1,8 +1,11 @@
 module types
 
+pub const command_origin_dev_console = u32(3)
+pub const command_origin_test = u32(4)
+
 pub struct CommandOriginData {
 pub mut:
-	type                   string
+	type                   u32
 	uuid                   UUID
 	request_id             string
 	player_actor_unique_id i64
